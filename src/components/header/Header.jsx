5d6d28c4cf679ccf4e1791b78d67ef94540e2b99
nikita-rawat <nikitarwt789@gmail.com>
2024-03-23 17:24:47 +0530

    update menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 import search from "../../assets/icons/search.svg";
 import user from "../../assets/icons/user.svg";
@@ -6,10 +6,13 @@ import cart from "../../assets/icons/cart.svg";
 import bag from "../../assets/icons/bag.svg";
 import bell from "../../assets/icons/bell.svg";
 import menu from "../../assets/icons/menu.svg";
+import close from "../../assets/icons/close.svg";
+
 
 
 
 const Header = () => {
+  const [isModalOpen, setIsModelOpen] = useState(false);
   return (
     <div className='header'>
       <div className="headerleft">
@@ -36,15 +39,28 @@ const Header = () => {
           <img src={bag} />
           <span>Seller</span>
         </div>
-        <div className='menu'>
+        <div className='menu' >
           <img src={bell} />
           <span>Notification</span>
         </div>
-        <div className='menuBtn'>
+        <div className='menuBtn' onClick={() => setIsModelOpen(true)}>
           <span>Menu</span>
           <img src={menu} />
         </div>
       </div>
+
+      {isModalOpen &&
+        <div className='profileModal' >
+          <button className='closeImg'>
+            <img src={close} className='closeIcon' alt='close' onClick={() => setIsModelOpen(false)} />
+          </button>
+          <div className='profileModalWrapper'>
+            <span className='profileWrapperSpan'>Login</span>
+            <span className='profileWrapperSpan'>Order</span>
+            <span className='profileWrapperSpan'>Cart</span>
+            <span className='profileWrapperSpan' style={{ backgroundColor: "red", color: "white", fontWeight: "500" }}>Logout</span>
+          </div>
+        </div>}
     </div>
   )
 }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -85,6 +85,8 @@ const Main = () => {
       </section>
 
 
+
+
     </div>
   )
 }
